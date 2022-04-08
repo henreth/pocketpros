3edfb53a5ec25897dddb9169152e90169454b7f9
Henry Miller <mail.henrym+git@gmail.com>
2022-04-07 22:41:08 -0400

    updated ultra pack method

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -116,38 +116,6 @@ class CardsController < ApplicationController
     render json: cards
     end
 
-    def ultra_pack
-        cards = []
-        len = 4
-
-        # Holo
-        rand_holo = rand() * 100
-        if rand_holo < 15
-            cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:3)
-        end
-
-        # Gold 
-        rand_gold1 = rand() * 100
-        if rand_gold1 < 35
-            cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:2)
-        end
-
-        rand_gold2 = rand() * 100
-        if rand_gold2 < 70
-            cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:2)
-        end
-
-        cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:2)
-
-        # Silver
-        newLength = len - cards.length;
-
-        newLength.times {|i| 
-        cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:1)
-    }
-    render json: cards
-    end
-
     def max_pack
         cards = []
         len = 6
@@ -179,5 +147,40 @@ class CardsController < ApplicationController
     }
     render json: cards
     end
+
+
+    def ultra_pack
+        cards = []
+        len = 4
+
+        # Holo
+        rand_holo = rand() * 100
+        if rand_holo < 15
+            cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:3)
+        end
+
+        # Gold 
+        rand_gold1 = rand() * 100
+        if rand_gold1 < 35
+            cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:2)
+        end
+
+        rand_gold2 = rand() * 100
+        if rand_gold2 < 70
+            cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:2)
+        end
+        
+        cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:2)
+        cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:2)
+
+        # Silver
+        newLength = len - cards.length;
+
+        newLength.times {|i| 
+        cards << Card.create!(user_id:@current_user.id,character_id: Character.all.sample.id,rarity:1)
+    }
+    render json: cards
+    end
+
 
 end     
