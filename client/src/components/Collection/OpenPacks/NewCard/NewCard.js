7f0e8c4f9a9834b6601e8ad1034bcfb6b84a74a3
Henry Miller <mail.henrym+git@gmail.com>
2022-04-10 18:19:13 -0400

    added requirement to click on the individual cards in order to reveal them

--- client/src/components/Collection/OpenPacks/NewCard/NewCard.js
+++ client/src/components/Collection/OpenPacks/NewCard/NewCard.js
@@ -7,11 +7,11 @@ export default function NewCard({ char, setCardElements }) {
     let [opened, setOpened] = useState(false)
 
     function handleClickCard() {
-        alert('working')
         setOpened(true)
     }
+    let rarity = char.rarity
 
-    let cardClass = opened ? 'charCard' : 'charCard'
+    let cardClass = opened ? `charCard ${rarity}` : 'charCard unopened'
 
     return (
         <div className={cardClass} onClick={handleClickCard}>
