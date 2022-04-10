9790d6e72804a00353349014c13c57a08f05cc8e
Henry Miller <mail.henrym+git@gmail.com>
2022-04-09 22:59:38 -0400

    demo styling for pack, functioning click to open back on backend with updating counts on front end

--- client/src/components/Collection/OpenPacks/OpenPacks.js
+++ client/src/components/Collection/OpenPacks/OpenPacks.js
@@ -2,6 +2,7 @@ import './OpenPacks.css';
 import React, { useState, useEffect } from 'react';
 import { Route, Routes, useNavigate } from "react-router-dom";
 import axios from 'axios';
+import icon from './floppydisk3.png'
 
 import Card from '../../Card/Card'
 
@@ -18,26 +19,39 @@ export default function OpenPacks({ user, setUser, setUserCards, userPacks, setU
     }, [])
 
 
-    function handleStudioClick(e){
-        axios.get(`/${e.target.textContent.toLowerCase()}_pack`)
-        .then(r=>console.log(r.data))
+    function handlePackClick(e) {
+        axios.get(`/studio_pack`)
+            .then(r => {
+                if (r.data.length > 15) {
+                    alert(r.data)
+                } else {
+                    console.log(r.data)
+                    fetch("/me")
+                    .then((r) => {
+                        if (r.ok) {
+                            r.json().then((user) => {
+                                setUser(user)
+                                setUserCards(user.cards)
+                                setUserPacks(user.packs)
+                            })
+                        }
+                    })        
+                }
 
-        fetch("/me")
-        .then((r) => {
-          if (r.ok) {
-            r.json().then((user) => {
-              setUser(user)
-              setUserCards(user.cards)
-              setUserPacks(user.packs)
-            })} 
-    })}
-    
+            })
+
+    }
+
 
     //{userPacks["booster"]}
     return (
         <div className='yourCards-container'>
-            <div value='studio' className="pack booster" onClick={handleStudioClick}>
-                STUDIO
+            <div className="pack booster" onClick={handlePackClick}>
+                <div className='pack-info-container'>
+                    <div>studio</div>
+                    <img className='floppy' src={icon} />
+                    <div>{userPacks["studio"]}</div>
+                </div>
             </div>
         </div>
     )
