cfb41f61607c9b84cd997f095c21717f6291b030
Henry Miller <mail.henrym+git@gmail.com>
2022-04-10 19:08:16 -0400

    added shuffling of opened pack cards on frontend

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
-import Modal from "../Modals/Modal";
 
 import './About.css'
 
--- client/src/components/Collection/OpenPacks/Modal/Modal.js
+++ client/src/components/Collection/OpenPacks/Modal/Modal.js
@@ -22,11 +22,21 @@ function Modal({ showModal, setShowModal, openedCards }) {
         setShowModal(false)
     }
 
-    let openedCardsToDisplay = openedCards.map(card => {
+    let openedCardsToDisplay = shuffle(openedCards).map(card => {
         return (
             <NewCard key={card.id} char={card} />
         )
     })
+
+    function shuffle(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    return array
+    }
 
     return (
         <div className="overlay" onBlur={handleClick}>
