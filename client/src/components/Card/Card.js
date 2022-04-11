3f4fed950ff7190ac3f4aadc1b3e1c6203deb7c7
Henry Miller <mail.henrym+git@gmail.com>
2022-04-11 17:47:58 -0400

    updated styling and added qr code

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,6 +4,8 @@ import hedy from '../../img/characters/hedylamarr.jpeg'
 import zuckerberg from '../../img/characters/markzuckerberg.jpeg'
 import alanturing from '../../img/characters/alanturing.jpeg'
 import stevejobs from '../../img/characters/stevejobs.jpeg'
+import icon from '../../img/clearqr.png';
+
 
 
 export default function Card({ char }) {
@@ -15,11 +17,13 @@ export default function Card({ char }) {
             <div className='charCard-yc-info-container'>
                 <img src={stevejobs} className='charCard-image' />
                 <div className='charCard-text'>
+                    <div className='charCard-rarity'>{char.rarity[0].toUpperCase() + char.rarity.slice(1,)}</div>
                     <div className='charCard-name'>{char.character.first_name} {char.character.last_name}</div>
-                    <div className='charCard-rarity'>{char.rarity[0].toUpperCase()+char.rarity.slice(1,)}</div>
-                    <div className='charCard-id'>Card #{char.id}</div>
+                    <div className='charCard-id'> #{char.id}</div>
                 </div>
             </div>
+            <img className='floppy-icon' src={icon} />
+
         </div>
     )
 }
