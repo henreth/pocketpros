c6b60250d36e48b491f08e2298c11723f50cc781
Henry Miller <mail.henrym+git@gmail.com>
2022-04-11 08:40:29 -0400

    charcard demo styling 1

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,5 +1,7 @@
 import './Card.css';
 import React, { useState } from 'react';
+import hedy from '../../img/characters/hedylamarr.jpeg'
+import zuckerberg from '../../img/characters/markzuckerberg.jpeg'
 
 
 export default function Card({ char }) {
@@ -8,8 +10,10 @@ export default function Card({ char }) {
 
     return (
         <div className={cardClass}>
-            <div className='charCard-info-container'>
-                <div>{char.rarity.toUpperCase()}</div>
+            <div className='charCard-yc-info-container'>
+                <div className='charCard-id'>#{char.id}</div>
+                <img src={zuckerberg} className='charCard-image'/>
+                {/* <div>{char.rarity.toUpperCase()}</div> */}
                 <div>{char.character.first_name} {char.character.last_name}</div>
             </div>
         </div>
