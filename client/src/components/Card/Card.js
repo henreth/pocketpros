46861d14c47e8153eb55e19c9d4f601f02b71d84
Henry Miller <mail.henrym+git@gmail.com>
2022-04-11 08:57:27 -0400

    updated newcard with new card styling

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import hedy from '../../img/characters/hedylamarr.jpeg'
 import zuckerberg from '../../img/characters/markzuckerberg.jpeg'
 import alanturing from '../../img/characters/alanturing.jpeg'
+import stevejobs from '../../../../img/characters/stevejobs.jpeg'
 
 
 export default function Card({ char }) {
@@ -12,7 +13,7 @@ export default function Card({ char }) {
     return (
         <div className={cardClass}>
             <div className='charCard-yc-info-container'>
-                <img src={hedy} className='charCard-image' />
+                <img src={stevejobs} className='charCard-image' />
                 <div className='charCard-text'>
                     <div className='charCard-name'>{char.character.first_name} {char.character.last_name}</div>
                     <div className='charCard-rarity'>{char.rarity[0].toUpperCase()+char.rarity.slice(1,)}</div>
--- client/src/components/Collection/OpenPacks/NewCard/NewCard.js
+++ client/src/components/Collection/OpenPacks/NewCard/NewCard.js
@@ -1,5 +1,10 @@
 import './NewCard.css';
 import React, { useState } from 'react';
+import hedy from '../../../../img/characters/hedylamarr.jpeg'
+import zuckerberg from '../../../../img/characters/markzuckerberg.jpeg'
+import alanturing from '../../../../img/characters/alanturing.jpeg'
+import stevejobs from '../../../../img/characters/stevejobs.jpeg'
+
 
 
 export default function NewCard({ char, setCardElements }) {
@@ -16,8 +21,12 @@ export default function NewCard({ char, setCardElements }) {
     return (
         <div className={cardClass} onClick={handleClickCard}>
             <div className='charCard-info-container'>
-                <div>{char.rarity.toUpperCase()}</div>
-                <div>{char.character.first_name} {char.character.last_name}</div>
+            <img src={stevejobs} className='charCard-image' />
+                <div className='charCard-text'>
+                    <div className='charCard-name'>{char.character.first_name} {char.character.last_name}</div>
+                    <div className='charCard-rarity'>{char.rarity[0].toUpperCase()+char.rarity.slice(1,)}</div>
+                    <div className='charCard-id'>Card #{char.id}</div>
+                </div>
             </div>
         </div>
     )
