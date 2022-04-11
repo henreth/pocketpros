6b7ea228a8a8e62e52cc062fe5e59bb1eb2dcd77
Henry Miller <mail.henrym+git@gmail.com>
2022-04-11 08:57:55 -0400

    fixed import issue

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import hedy from '../../img/characters/hedylamarr.jpeg'
 import zuckerberg from '../../img/characters/markzuckerberg.jpeg'
 import alanturing from '../../img/characters/alanturing.jpeg'
-import stevejobs from '../../../../img/characters/stevejobs.jpeg'
+import stevejobs from '../../img/characters/stevejobs.jpeg'
 
 
 export default function Card({ char }) {
