8af5889d4f7a3ef81e0b4967fed7b0960146f0ef
Henry Miller <mail.henrym+git@gmail.com>
2022-04-14 00:34:00 -0400

    edit profile details complete

--- client/src/components/Auth/Edit/Edit.js
+++ client/src/components/Auth/Edit/Edit.js
@@ -4,7 +4,7 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 
-export default function Edit({ username, setUsername, signUpFirstName, setSignUpFirstName, signUpLastName, setSignUpLastName, signedIn, handleLogOut }) {
+export default function Edit({ user, setUser, signedIn, handleLogOut }) {
   document.title = 'Pocket Pros - Edit Account Details';
   let navigate = useNavigate();
 
@@ -12,7 +12,50 @@ export default function Edit({ username, setUsername, signUpFirstName, setSignUp
     if (signedIn === false) {
       navigate('/');
     }
-  }, [])
+  }, [])  
+
+
+    const [username,setUsername] = useState(signedIn?user.username:null)
+    const [firstName, setFirstName] = useState(signedIn?user.first_name:null)
+    const [lastName, setLastName] = useState(signedIn?user.last_name:null)
+
+
+  function handleSubmitChanges(e) {
+    e.preventDefault();
+    if (username === '') {
+        setUsername(user.username)
+    }
+
+    if (firstName === '') {
+        setFirstName(user.first_name)
+    }
+
+    if (lastName === '') {
+        setLastName(user.last_name)
+    }
+
+    let updatedDetails = {
+        "first_name": firstName,
+        "last_name": lastName,
+        username
+    }
+
+    axios.patch('/users/' + user.id, updatedDetails)
+        .then(r => {
+            setUser(r.data)
+            alert('Your account has been updated!')
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data.errors);
+                alert(error.response.data.errors)
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log('Error', error.message);
+            }
+        });
+}
 
 
   return (
@@ -25,26 +68,26 @@ export default function Edit({ username, setUsername, signUpFirstName, setSignUp
           </div>
           <form className="log-in-container">
             <div className='signup-input-fullname'>
-              <input className='signup-input-name' type="text" placeholder="First Name" value={signUpFirstName} onChange={(e) => { setSignUpFirstName(e.target.value) }} />
-              <input className='signup-input-name last' type="text" placeholder="Last Name" value={signUpLastName} onChange={(e) => { setSignUpLastName(e.target.value) }} />
+              <input className='signup-input-name' type="text" placeholder="First Name" value={firstName} onChange={(e) => { setFirstName(e.target.value) }} />
+              <input className='signup-input-name last' type="text" placeholder="Last Name" value={lastName} onChange={(e) => { setLastName(e.target.value) }} />
             </div>
             <input className='signup-input' type="text" placeholder="Email" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <button className='signup-button' onClick={null} >Submit Changes</button>
+            <button className='signup-button' onClick={handleSubmitChanges} >Submit Changes</button>
           </form>
         </div>
-        <div className="menu__slot" onClick={handleLogOut}>
+        {/* <div className="menu__slot" onClick={handleLogOut}>
             <div className="blackscreen"></div>
             <div className="char eich"></div>
             <div className="slot-item-3">
               <h3>Log Out</h3>
               <p></p>
             </div>
-          </div>
-          <div className="menu__slot" onClick={()=>{navigate('/')}}>
+          </div> */}
+          <div className="menu__slot" onClick={()=>{navigate('/profile')}}>
             <div className="blackscreen"></div>
-            <div className="char larry"></div>
-            <div className="slot-item-4">
-              <h3>Back</h3>
+            <div className="char zuckerberg"></div>
+            <div className="slot-item-2">
+              <h3>Cancel</h3>
               <p></p>
             </div>
           </div>
