03696ed379df6209a87d9c61bdfb5be8e1e4db36
Henry Miller <mail.henrym+git@gmail.com>
2022-04-14 01:03:23 -0400

    added title card to log in page

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -27,11 +27,11 @@ export default function Auth({signedIn,setSignedIn}) {
   return (
     <React.Fragment>
       <div id="menu">
-        {/* <div className="title__slot" >
+        <div className="title__slot" >
           <div className="blackscreen"></div>
           <div className=""></div>
-          <div className="slot-item-1">
-            <h3>Pocket</h3>
+          <div className="slot-item-2">
+            <h3 className='first'>Pocket</h3>
             <p></p>
           </div>
         </div>
@@ -39,14 +39,14 @@ export default function Auth({signedIn,setSignedIn}) {
           <div className="blackscreen"></div>
           <div className=""></div>
           <div className="slot-item-2">
-            <h3>Programmers</h3>
+            <h3 className='second'>Programmers</h3>
             <p></p>
           </div>
-        </div> */}
+        </div>
         <div className="menu__slot" onClick={handleLogInClick}>
           <div className="blackscreen"></div>
           <div className="char jobs"></div>
-          <div className="slot-item-1">
+          <div className="slot-item-3">
             <h3>Log In</h3>
             <p></p>
           </div>
@@ -54,7 +54,7 @@ export default function Auth({signedIn,setSignedIn}) {
         <div className="menu__slot" onClick={handleSignUpClick}>
           <div className="blackscreen"></div>
           <div className="char kjohnson"></div>
-          <div className="slot-item-2">
+          <div className="slot-item-4">
             <h3>Sign Up</h3>
             <p></p>
           </div>
