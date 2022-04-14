c4ff526639cab2a3aaa9a0bf4311b890d0b451fb
Henry Miller <mail.henrym+git@gmail.com>
2022-04-13 22:35:04 -0400

    updated migrations

--- db/migrate/20220406180610_create_cards.rb
+++ db/migrate/20220406180610_create_cards.rb
@@ -5,7 +5,8 @@ class CreateCards < ActiveRecord::Migration[6.1]
       t.references :character, null: false, foreign_key: true
       t.integer :rarity
       t.boolean :for_sale
-      t.integer :value
+      t.integer :sale_price
+      t.string :unique_id
 
       t.timestamps
     end
