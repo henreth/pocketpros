77f41ff9d089e1ea0ef9c1a9afdbe1ecdf9bbefa
Henry Miller <mail.henrym+git@gmail.com>
2022-04-15 21:51:55 -0400

    background for market details added

--- client/src/components/Collection/YourCards/CardInformation/CardInformation.js
+++ client/src/components/Collection/YourCards/CardInformation/CardInformation.js
@@ -23,22 +23,23 @@ export default function CardInformation({ selectedCard, showModal, setShowModal 
     return (
         <React.Fragment>
             <div className="overlay" onBlur={handleClick}>
-            <button className= 'button openPacks' onClick={handleClick}>cancel</button>
-            <div className='cardInformation-container'>
-                <div className={cardClass}>
-                <div className='charCard-info-container' >
-                    <img src={charImages('./'+selectedCard.character.image_url)} className='charCard-image' />
-                    <div className='charCard-text'>
-                        <div className='charCard-rarity'>{selectedCard.rarity}</div>
-                        <div className='charCard-name'><b>{selectedCard.character.first_name} {selectedCard.character.last_name}</b></div>
-                        <div className='charCard-id'> {selectedCard.unique_id}</div>
+                <button className='button openPacks' onClick={handleClick}>cancel</button>
+                <div className='cardInformation-container'>
+                    <div className={cardClass}>
+                        <div className='charCard-info-container' >
+                            <img src={charImages('./' + selectedCard.character.image_url)} className='charCard-image' />
+                            <div className='charCard-text'>
+                                <div className='charCard-rarity'>{selectedCard.rarity}</div>
+                                <div className='charCard-name'><b>{selectedCard.character.first_name} {selectedCard.character.last_name}</b></div>
+                                <div className='charCard-id'> {selectedCard.unique_id}</div>
+                            </div>
+                        </div>
+                        <img className='floppy-icon' src={icon} />
                     </div>
-                </div>
-                <img className='floppy-icon' src={icon} />
-        </div>
+                    <div className='market-information-container'></div>
 
+                </div>
             </div>
-        </div>
 
         </React.Fragment>
     );
