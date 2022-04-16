9bd3b3f5f23239c4935145eedf3ce9a1a21d74a7
Henry Miller <mail.henrym+git@gmail.com>
2022-04-16 17:50:24 -0400

    updated find transactions to remove nils

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,7 @@ class TransactionsController < ApplicationController
         @char_id = params[:char_id]
         @card_rarity = params[:rarity]
 
-        all_transactions = Transaction.all.filter{|transaction| transaction.card.character.id == @char_id && transaction.card.rarity == @card_rarity}
+        all_transactions = Transaction.all.filter{|transaction| transaction.card.character.id == @char_id && transaction.card.rarity == @card_rarity && transaction.sale_price != nil} 
 
         render json: all_transactions
     end
